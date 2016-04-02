df486bc21f65d3df96366aaa7df14d3c896ebff3
abeltanjq <abeltanjq@gmail.com>
2016-04-02 19:52:37 +0800

    Setting up test scene. 1. Destination "sphere" is draggable with mouse. 2. Still setting up a follower to reach Destination

---
+++ FABRIK-v01/Assets/follower.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class follower : MonoBehaviour {
+
+	GameObject des;
+	Transform target;
+	// Use this for initialization
+	void Start () {
+		des = GameObject.Find ("Destination");
+	}
+
+	void reachForDes () {
+		target = des.transform;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 moveToDes = target.position - this.transform.LocalPosition;
+
+	}
+}
---
+++ FABRIK-v01/Assets/mouseDrag.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mouseDrag : MonoBehaviour {
+	float distance = 10;
+	// Use this for initialization
+	void OnMouseDrag () {
+		Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+		Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+		transform.position = objPosition;
+		Debug.Log("mousePosition x = " + Input.mousePosition.x + ", y = "+ Input.mousePosition.y);
+	}
+}
